9d061491b1d9cdd5c701ee44ff49583ab802e8bf
makr0hard <n0ne@really.fr>
2026-08-15 14:36:43 +0200

    stuff

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <QApplication>
 
 int main(int argc, char *argv[])
-{
+{// I'll have to the address i use because qtcreator doesn't let you commit with whatever address and if a company named really goes into emails, i'm cooked
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
